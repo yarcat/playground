d59ec52cb1d640221c241e7b50c7f41fba6b9e56
Jaroslavs Samcuks <yarcat@gmail.com>
2020-04-30 17:32:12 +0200

    create options on evry presenter iteration

--- geom/body/presenter.go
+++ geom/body/presenter.go
@@ -4,14 +4,13 @@ import "github.com/hajimehoshi/ebiten"
 
 // Present draws bodies on the screen.
 func Present(image *ebiten.Image, it Iterator) {
-	op := &ebiten.DrawImageOptions{}
 	for it.Next() {
 		b := it.Value()
 		h, w := b.Image.Size()
 		dx := b.Pos.X - float64(h)/2
 		dy := b.Pos.Y - float64(w)/2
+		op := &ebiten.DrawImageOptions{}
 		op.GeoM.Translate(dx, dy)
 		image.DrawImage(b.Image, op)
-		op.GeoM.Translate(-dx, dy)
 	}
 }
