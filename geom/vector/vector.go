d7ed49c45cc7609e8ede83a2802279e7415a33a0
Jaroslavs Samcuks <yarcat@gmail.com>
2020-05-12 01:10:26 +0200

    vector add/sub

--- geom/vector/vector.go
+++ geom/vector/vector.go
@@ -2,6 +2,8 @@
 // cross products.
 package vector
 
+import "math"
+
 // Vector represents a geometric object that has magnitude and direction.
 type Vector struct {
 	X, Y float64
@@ -10,4 +12,20 @@ type Vector struct {
 // New returns new vector pointing to the provided point.
 func New(X, Y float64) Vector {
 	return Vector{X, Y}
+}
+
+// Rotate returns new vector rotated by the specified rad.
+func (v Vector) Rotate(a float64) Vector {
+	cos, sin := math.Cos(a), math.Sin(a)
+	return New(v.X*cos-v.Y*sin, v.X*sin+v.Y*cos)
+}
+
+// Add adds a to v.
+func (v Vector) Add(a Vector) Vector {
+	return New(v.X+a.X, v.Y+a.Y)
+}
+
+// Sub subtracts a from v.
+func (v Vector) Sub(a Vector) Vector {
+	return New(v.X-a.X, v.Y-a.Y)
 }
