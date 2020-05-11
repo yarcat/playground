ea06d20c0ec3568aa7ea7e68cf83c722910661a3
Jaroslavs Samcuks <yarcat@gmail.com>
2020-05-12 01:10:47 +0200

    draw triangle

--- geom/app/main.go
+++ geom/app/main.go
@@ -4,7 +4,9 @@ import (
 	"image"
 	"image/color"
 	"log"
+	"math"
 
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 	"github.com/yarcat/playground/geom/app/application"
 	"github.com/yarcat/playground/geom/app/component/button"
 	"github.com/yarcat/playground/geom/app/component/canvas"
@@ -12,6 +14,7 @@ import (
 	"github.com/yarcat/playground/geom/app/component/features"
 	"github.com/yarcat/playground/geom/app/component/label"
 	"github.com/yarcat/playground/geom/shapes"
+	"github.com/yarcat/playground/geom/vector"
 )
 
 func main() {
@@ -81,6 +84,35 @@ func main() {
 	})
 	r.SetBounds(image.Rect(500, 500, 600, 600))
 	app.AddComponent(drag.EnableFor(r))
+
+	t := canvas.New(func(img *canvas.Image) {
+		if img.Invalidated() {
+			w, h := img.Size()
+			x0, y0 := float64(w)/2, float64(h)/2
+			side := float64(w)
+			if float64(h) < side {
+				side = float64(h)
+			}
+			p0 := vector.New(-side/2, -y0+math.Sqrt(0.75*side*side)/6)
+			p := vector.New(side, 0)
+			v := []vector.Vector{
+				p0,
+				p.Add(p0),
+				p.Rotate(math.Pi / 3).Add(p0),
+			}
+			edges := [...][2]int{{0, 1}, {1, 2}, {2, 0}}
+			for _, e := range edges {
+				ebitenutil.DrawLine(
+					img.Image,
+					x0+v[e[0]].X, y0+v[e[0]].Y,
+					x0+v[e[1]].X, y0+v[e[1]].Y,
+					color.White,
+				)
+			}
+		}
+	})
+	t.SetBounds(image.Rect(500, 500, 600, 600))
+	app.AddComponent(drag.EnableFor(t))
 
 	if err := application.Run(app); err != nil {
 		log.Fatalf("Run failed: %v", err)
