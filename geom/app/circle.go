771278e1119c2329ebc5207b19045acf471ca8c9
Jaroslavs Samcuks <yarcat@gmail.com>
2020-05-12 21:43:12 +0200

    circle refactoring

--- geom/app/circle.go
+++ geom/app/circle.go
@@ -6,33 +6,37 @@ import (
 	"math"
 
 	"github.com/yarcat/playground/geom/app/application"
-	"github.com/yarcat/playground/geom/app/component"
 	"github.com/yarcat/playground/geom/app/component/canvas"
 	"github.com/yarcat/playground/geom/app/component/drag"
 	"github.com/yarcat/playground/geom/app/intersect"
 	"github.com/yarcat/playground/geom/shapes"
 )
 
-var circles = make(map[component.Component]*intersect.C)
+var circles []*circle
+
+type circle struct {
+	intersect.C
+}
+
+func (c *circle) draw(img *canvas.Image) {
+	img.Clear()
+	img.Fill(color.RGBA{0xff, 0xff, 0xff, 0xa0})
+	w, h := img.Size()
+	var col color.Color = color.White
+	for _, other := range circles {
+		if other == c {
+			continue
+		}
+		if _, ok := intersect.Circles(c.C, other.C); ok {
+			col = color.RGBA{0xff, 0, 0, 0xff}
+		}
+	}
+	shapes.DrawCircle(img.Image, w/2, h/2, int(c.R), col)
+}
 
 func addCircle(app *application.App, x, y, r int, updateStatus func(image.Rectangle)) {
-	xc := &intersect.C{X: float64(x), Y: float64(y), R: float64(r)}
-	var c *canvas.Canvas
-	c = canvas.New(func(img *canvas.Image) {
-		img.Clear()
-		img.Fill(color.RGBA{0xff, 0xff, 0xff, 0xa0})
-		w, h := img.Size()
-		var col color.Color = color.White
-		for otherc, otherxc := range circles {
-			if otherc == c {
-				continue
-			}
-			if _, ok := intersect.Circles(*xc, *otherxc); ok {
-				col = color.RGBA{0xff, 0, 0, 0xff}
-			}
-		}
-		shapes.DrawCircle(img.Image, w/2, h/2, r, col)
-	})
+	xc := circle{C: intersect.C{X: float64(x), Y: float64(y), R: float64(r)}}
+	c := canvas.New(xc.draw)
 	c.SetBounds(image.Rect(x-r, y-r, x+r, y+r))
 	d := drag.EnableFor(c)
 	d.AddDragListener(func() {
@@ -43,6 +47,6 @@ func addCircle(app *application.App, x, y, r int, updateStatus func(image.Rectan
 			math.Round(float64(b.Min.Y+b.Max.Y)/2),
 		)
 	})
-	circles[c] = xc
+	circles = append(circles, &xc)
 	app.AddComponent(d)
 }
