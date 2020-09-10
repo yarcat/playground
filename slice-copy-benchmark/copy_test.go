c0993104871166af7ee2d62aea871845b6864715
Jaroslavs Samcuks <yarcat@gmail.com>
2020-09-10 17:28:43 +0200

    Also test index; added transform tests.
    Added another test option:
     - preallocate with cap set to len
     - use indices and values to copy
    
    Added transform tests for every group to ensure we can compare apples to
    apples.

--- slice-copy-benchmark/copy_test.go
+++ slice-copy-benchmark/copy_test.go
@@ -9,15 +9,44 @@ var (
 	large  = make([]int, 1_000_000)
 )
 
-func BenchmarkSliceCopyWithAppendEmpty(b *testing.B)  { benchmark(b, sliceCopyWithAppend, empty) }
-func BenchmarkSliceCopyWithAppendSmall(b *testing.B)  { benchmark(b, sliceCopyWithAppend, small) }
-func BenchmarkSliceCopyWithAppendMedium(b *testing.B) { benchmark(b, sliceCopyWithAppend, medium) }
-func BenchmarkSliceCopyWithAppendLarge(b *testing.B)  { benchmark(b, sliceCopyWithAppend, large) }
+func BenchmarkSliceCopyWithAppendEmpty(b *testing.B) {
+	benchmark(b, sliceCopyWithAppend, empty)
+}
+func BenchmarkSliceCopyWithAppendSmall(b *testing.B) {
+	benchmark(b, sliceCopyWithAppend, small)
+}
+func BenchmarkSliceCopyWithAppendMedium(b *testing.B) {
+	benchmark(b, sliceCopyWithAppend, medium)
+}
+func BenchmarkSliceCopyWithAppendLarge(b *testing.B) {
+	benchmark(b, sliceCopyWithAppend, large)
+}
 
-func BenchmarkSliceCopyWithPreallocEmpty(b *testing.B)  { benchmark(b, sliceCopyWithPrealloc, empty) }
-func BenchmarkSliceCopyWithPreallocSmall(b *testing.B)  { benchmark(b, sliceCopyWithPrealloc, small) }
-func BenchmarkSliceCopyWithPreallocMedium(b *testing.B) { benchmark(b, sliceCopyWithPrealloc, medium) }
-func BenchmarkSliceCopyWithPreallocLarge(b *testing.B)  { benchmark(b, sliceCopyWithPrealloc, large) }
+func BenchmarkSliceCopyWithTransformAppendEmpty(b *testing.B) {
+	benchmark(b, sliceCopyWithTransformAppend(transform), empty)
+}
+func BenchmarkSliceCopyWithTransformAppendSmall(b *testing.B) {
+	benchmark(b, sliceCopyWithTransformAppend(transform), small)
+}
+func BenchmarkSliceCopyWithTransformAppendMedium(b *testing.B) {
+	benchmark(b, sliceCopyWithTransformAppend(transform), medium)
+}
+func BenchmarkSliceCopyWithTransformAppendLarge(b *testing.B) {
+	benchmark(b, sliceCopyWithTransformAppend(transform), large)
+}
+
+func BenchmarkSliceCopyWithPreallocEmpty(b *testing.B) {
+	benchmark(b, sliceCopyWithPrealloc, empty)
+}
+func BenchmarkSliceCopyWithPreallocSmall(b *testing.B) {
+	benchmark(b, sliceCopyWithPrealloc, small)
+}
+func BenchmarkSliceCopyWithPreallocMedium(b *testing.B) {
+	benchmark(b, sliceCopyWithPrealloc, medium)
+}
+func BenchmarkSliceCopyWithPreallocLarge(b *testing.B) {
+	benchmark(b, sliceCopyWithPrealloc, large)
+}
 
 func BenchmarkSliceCopyWithTransformPreallocEmpty(b *testing.B) {
 	benchmark(b, sliceCopyWithTransformPrealloc(transform), empty)
@@ -30,6 +59,32 @@ func BenchmarkSliceCopyWithTransformPreallocMedium(b *testing.B) {
 }
 func BenchmarkSliceCopyWithTransformPreallocLarge(b *testing.B) {
 	benchmark(b, sliceCopyWithTransformPrealloc(transform), large)
+}
+
+func BenchmarkSliceCopyWithIndexAndPreallocEmpty(b *testing.B) {
+	benchmark(b, sliceCopyWithIndexAndPrealloc, empty)
+}
+func BenchmarkSliceCopyWithIndexAndPreallocSmall(b *testing.B) {
+	benchmark(b, sliceCopyWithIndexAndPrealloc, small)
+}
+func BenchmarkSliceCopyWithIndexAndPreallocMedium(b *testing.B) {
+	benchmark(b, sliceCopyWithIndexAndPrealloc, medium)
+}
+func BenchmarkSliceCopyWithIndexAndPreallocLarge(b *testing.B) {
+	benchmark(b, sliceCopyWithIndexAndPrealloc, large)
+}
+
+func BenchmarkSliceCopyWithTransformIndexAndPreallocEmpty(b *testing.B) {
+	benchmark(b, sliceCopyWithTransformIndexAndPrealloc(transform), empty)
+}
+func BenchmarkSliceCopyWithTransformIndexAndPreallocSmall(b *testing.B) {
+	benchmark(b, sliceCopyWithTransformIndexAndPrealloc(transform), small)
+}
+func BenchmarkSliceCopyWithTransformIndexAndPreallocMedium(b *testing.B) {
+	benchmark(b, sliceCopyWithTransformIndexAndPrealloc(transform), medium)
+}
+func BenchmarkSliceCopyWithTransformIndexAndPreallocLarge(b *testing.B) {
+	benchmark(b, sliceCopyWithTransformIndexAndPrealloc(transform), large)
 }
 
 type copyFunc func(in []int) (out []int)
@@ -71,4 +126,37 @@ func sliceCopyWithAppend(in []int) (out []int) {
 		out = append(out, v)
 	}
 	return
+}
+
+func sliceCopyWithTransformAppend(fn func(int) int) copyFunc {
+	return func(in []int) (out []int) {
+		for _, v := range in {
+			out = append(out, v)
+		}
+		return
+	}
+}
+
+func sliceCopyWithIndexAndPrealloc(in []int) (out []int) {
+	if len(in) == 0 {
+		return nil
+	}
+	out = make([]int, len(in))
+	for i, v := range in {
+		out[i] = v
+	}
+	return
+}
+
+func sliceCopyWithTransformIndexAndPrealloc(fn func(int) int) copyFunc {
+	return func(in []int) (out []int) {
+		if len(in) == 0 {
+			return nil
+		}
+		out = make([]int, len(in))
+		for i, v := range in {
+			out[i] = v
+		}
+		return
+	}
 }
