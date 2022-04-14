3ab8082f6b0b09e991d5fd15bffdc8b73191b95d
Jaroslavs Samcuks <yarcat@gmail.com>
2022-04-14 09:28:07 +0200

    modernc-sqlite: factored out functions

--- modernc-sqlite/main.go
+++ modernc-sqlite/main.go
@@ -9,25 +9,53 @@ import (
 )
 
 func main() {
+	db := mustOpenDB("sqlite", ":memory:")
+	defer db.Close()
+	mustCreateTables(db)
+	mustInsertNames(db, "foo", "bar", "larch", "spam", "egg")
+	mustPrintAllNames(db)
+
+	fmt.Println("Hello World")
+}
+
+func mustOpenDB(driver, source string) *sql.DB {
 	db, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
 		log.Fatalf("unable to open sqlite connection: %v", err)
 	}
+	return db
+}
 
-	const createTableStmt = `CREATE TABLE test (
+const (
+	createTableStmt = `CREATE TABLE test (
 		id 		INTEGER PRIMARY KEY AUTOINCREMENT,
 		name 	TEXT
 	)`
+	insertStmt = `INSERT INTO test (name) VALUES (?)`
+	selectStmt = `SELECT id, name FROM test`
+)
+
+func mustCreateTables(db *sql.DB) {
 	if _, err := db.Exec(createTableStmt); err != nil {
 		log.Fatalf("unable to create table: %v", err)
 	}
+}
 
-	const insertStmt = `INSERT INTO test (name) VALUES ("Foo"), ("Bar")`
-	if _, err := db.Exec(insertStmt); err != nil {
-		log.Fatalf("unable to insert: %v", err)
+func mustInsertNames(db *sql.DB, names ...string) {
+	px, err := db.Prepare(insertStmt)
+	if err != nil {
+		log.Fatalf("unable to prepare insert: %v", err)
 	}
+	defer px.Close()
 
-	const selectStmt = `SELECT id, name FROM test`
+	for _, name := range names {
+		if _, err := px.Exec(name); err != nil {
+			log.Fatalf("unable to exec prepared insert: %v", err)
+		}
+	}
+}
+
+func mustPrintAllNames(db *sql.DB) {
 	rows, err := db.Query(selectStmt)
 	if err != nil {
 		log.Fatalf("unable to execute select: %v", err)
@@ -42,8 +70,6 @@ func main() {
 		if err := rows.Scan(&id, &name); err != nil {
 			log.Fatalf("unable to scan: %v", err)
 		}
-		fmt.Printf("Scanned rows with id=%v name=%q\n", id, name)
+		fmt.Printf("Scanned row with id=%v name=%q\n", id, name)
 	}
-
-	fmt.Println("Hello World")
 }
