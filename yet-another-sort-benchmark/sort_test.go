5c16881695d78c9baa8e2e32fd1859bb3d378677
Jaroslavs Samcuks <yarcat@gmail.com>
2022-08-11 20:47:54 +0200

    add test, fix bugs

--- yet-another-sort-benchmark/sort_test.go
+++ yet-another-sort-benchmark/sort_test.go
@@ -2,6 +2,7 @@ package sort_test
 
 import (
 	"math/rand"
+	"reflect"
 	"testing"
 
 	"golang.org/x/exp/constraints"
@@ -27,6 +28,41 @@ func benchmarkSort(b *testing.B, sort func([]int)) {
 func BenchmarkSort(b *testing.B)       { benchmarkSort(b, Sort[int]) }
 func BenchmarkSortBubble(b *testing.B) { benchmarkSort(b, SortBubble[int]) }
 
+func testSort(t *testing.T, sort func([]int)) {
+	for _, tc := range []struct {
+		name        string
+		input, want []int
+	}{
+		{"nil", nil, nil},
+		{"empty", []int{}, []int{}},
+		{"single", []int{123}, []int{123}},
+		{"two_sorted", []int{1, 2}, []int{1, 2}},
+		{"two_reversed", []int{2, 1}, []int{1, 2}},
+		{"three_random", []int{2, 3, 1}, []int{1, 2, 3}},
+		{"three_sorted", []int{1, 2, 3}, []int{1, 2, 3}},
+		{"three_reversed", []int{3, 2, 1}, []int{1, 2, 3}},
+		{"four_reversed", []int{4, 3, 2, 1}, []int{1, 2, 3, 4}},
+		{"ten_random", []int{4, 3, 7, 1, 0, 5, 2, 6, 9, 8}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+		{"ten_sorted", []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+		{"ten_reversed", []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var got []int
+			if tc.input != nil {
+				got = make([]int, len(tc.input))
+				copy(got, tc.input)
+			}
+			sort(got)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("got = %v, want = %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestSort(t *testing.T)       { testSort(t, Sort[int]) }
+func TestSortBubble(t *testing.T) { testSort(t, SortBubble[int]) }
+
 func min(a, b int) int {
 	if b < a {
 		return b
@@ -35,8 +71,7 @@ func min(a, b int) int {
 }
 
 func Sort[T constraints.Ordered](s []T) {
-	x := make([]T, len(s))
-	from, to, fromS := s, x, true
+	from, to, fromS := s, make([]T, len(s)), true
 	merge := func(seg, out int) int {
 		a, b := out, out+seg
 		A, B := min(seg, len(s)-a), min(seg, len(s)-b)
@@ -62,8 +97,8 @@ func Sort[T constraints.Ordered](s []T) {
 		}
 		from, to, fromS = to, from, !fromS
 	}
-	if fromS {
-		copy(s, to)
+	if !fromS {
+		copy(s, from)
 	}
 }
 
