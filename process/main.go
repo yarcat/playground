df23909b5c8c077d592f208446527212949d33ca
Jaroslavs Samcuks <yarcat@gmail.com>
2024-05-15 15:31:55 +0200

    dispatch signalled terminations

--- process/main.go
+++ process/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
+	"syscall"
 	"time"
 )
 
@@ -27,20 +30,29 @@ func NewWorker(ctx context.Context) (*Worker, error) {
 
 func (w *Worker) Wait() error { return w.cmd.Wait() }
 
-func runPool(ctx context.Context, n int) error {
+func runPool(ctx context.Context, n int) {
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
 	workers := make(chan struct{}, n)
 	for {
 		select {
 		case <-ctx.Done():
-			return nil
+			return
 		case workers <- struct{}{}:
+			wg.Add(1)
 			go func() {
+				defer wg.Done()
 				defer func() { <-workers }()
 				w, err := NewWorker(ctx)
 				if err == nil {
 					err = w.Wait()
 				}
 				fmt.Println(err)
+				var e *exec.ExitError
+				if errors.As(err, &e) {
+					fmt.Println(e.Sys().(syscall.WaitStatus).Signaled())
+				}
 			}()
 		}
 	}
@@ -48,6 +60,6 @@ func runPool(ctx context.Context, n int) error {
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
-	defer time.AfterFunc(20*time.Second, cancel).Stop()
-	fmt.Println(runPool(ctx, 5))
+	defer time.AfterFunc(5*time.Second, cancel).Stop()
+	runPool(ctx, 5)
 }
